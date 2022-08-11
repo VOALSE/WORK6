45aeae73de264d24aec263f6d68ca4fa8bb78440
Alexandr <sashkavoytov20@gmail.com>
2022-08-11 23:01:10 +0500

    Выполнил 6ю домашнюю работу

---
+++ task41/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int[] CreateArray(int size) //Ввод массива с клавиатуры
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введи {i + 1} число: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+int GetSumPosElem(int[] array) //Массив считает количество положительных элементов массива
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[] array) //Печать массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
+    }
+}
+
+Console.Clear();
+Console.Write("Сколько чисел хотите ввести? ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+int[] array = CreateArray(sizeArray);
+PrintArray(array);
+Console.WriteLine();
+int SumPosElem = GetSumPosElem(array);
+Console.WriteLine($"Количество чисел больше 0 =  {SumPosElem} ");
---
+++ task43/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Clear();
+
+Console.WriteLine("Введите значения первого отрезка ");
+Console.Write("b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значения второго отрезка ");
+Console.Write("b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+
+double InterTwoLines(double x, double first, double second)
+{
+    double y = first * x + second;
+    return y;
+}
+
+double y1 = InterTwoLines(x, k1, b1);
+double y2 = InterTwoLines(x, k2, b2);
+
+Console.WriteLine($"Точка пересечения -> ({Math.Round(y1,1)}; {Math.Round(y2,1)})");
